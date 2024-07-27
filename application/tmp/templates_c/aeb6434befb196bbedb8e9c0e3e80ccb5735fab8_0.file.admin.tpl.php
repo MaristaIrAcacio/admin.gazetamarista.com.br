78d86f46e4bc68266d7de555240e6fbc87c3a99b
vitorgabrieldev <vitorgabrieldeoliveiradev@gmail.com>
2024-07-27 14:52:29 -0300

    feat: :sparkles: Feito

--- application/tmp/templates_c/aeb6434befb196bbedb8e9c0e3e80ccb5735fab8_0.file.admin.tpl.php
+++ application/tmp/templates_c/aeb6434befb196bbedb8e9c0e3e80ccb5735fab8_0.file.admin.tpl.php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 3.1.36, created on 2024-07-27 14:48:08
+/* Smarty version 3.1.36, created on 2024-07-27 14:52:20
   from 'c:\xampp-7.4\htdocs\admin.gazetamarista.com.br\application\layouts\admin.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.36',
-  'unifunc' => 'content_66a532d89b2ef1_84899636',
+  'unifunc' => 'content_66a533d49fc611_64405540',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_66a532d89b2ef1_84899636 (Smarty_Internal_Template $_smarty_tpl) {
+function content_66a533d49fc611_64405540 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!doctype html>
 <html class="no-js" lang="pt">
 	<head>
